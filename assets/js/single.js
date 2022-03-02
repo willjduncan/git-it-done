cd0cf8676fd75b639a8c05e090f6c7e0c204b2f2
Will Duncan <willduncan@MBP-8323.attlocal.net>
2022-03-02 15:18:20 -0500

    add functionality to repos list

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,13 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+ var queryString = document.location.search;
+ var repoName = queryString.split("=")[1];
+ getRepoIssues(repoName);
+ repoNameEl.textContent = repoName;
+}
 
 var getRepoIssues = function(repo) {
     console.log(repo);
@@ -10,6 +19,10 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data) {
             // pass response data to dom function
             displayIssues(data); 
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo)
+                }
             });
         } else {
             alert("There was a problem with your request!")
@@ -50,9 +63,20 @@ var displayIssues = function(issues) {
         issueEl.appendChild(typeEl);
         issueContainerEl.appendChild(issueEl);
     }
-
 };
 
 
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+  };
 
-  getRepoIssues("facebook/react");
+
+getRepoName();
