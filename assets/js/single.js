80e50a7c0f22463a99308879f8f037a6e774fd49
Will Duncan <willduncan@MBP-8323.attlocal.net>
2022-03-02 15:31:41 -0500

    debug

--- assets/js/single.js
+++ assets/js/single.js
@@ -3,29 +3,39 @@ var limitWarningEl = document.querySelector("#limit-warning");
 var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoName = function() {
- var queryString = document.location.search;
- var repoName = queryString.split("=")[1];
- getRepoIssues(repoName);
- repoNameEl.textContent = repoName;
-}
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+  
+    if (repoName) {
+      // display repo name on the page
+      repoNameEl.textContent = repoName;
+  
+      getRepoIssues(repoName);
+    } else {
+      // if no repo was given, redirect to the homepage
+      document.location.replace("./index.html");
+    }
+  };
 
 var getRepoIssues = function(repo) {
-    console.log(repo);
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
-    fetch(apiUrl)
-    .then(function(response){
+    // make a get request to url
+    fetch(apiUrl).then(function(response) {
+     // request was successful
         if (response.ok) {
             response.json().then(function(data) {
-            // pass response data to dom function
-            displayIssues(data); 
+                displayIssues(data);
+        
                 // check if api has paginated issues
                 if (response.headers.get("Link")) {
-                    displayWarning(repo)
+                 displayWarning(repo);
                 }
             });
         } else {
-            alert("There was a problem with your request!")
+        // if not successful, redirect to homepage
+        document.location.replace("./index.html");
         }
     });
 }  
